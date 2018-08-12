0a260cfe1e48858d901f646ed671fb37d09987ff
Brandon Asuncion <me@brandonasuncion.tech>
2018-08-12 10:52:08 -0700

    Cleanup backend

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ var io = require('socket.io')(server);
 
 var lastBroadcastedData = {};
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
 
     socket.on('room', function(room) {
         if (room == null || room === "") {
@@ -30,7 +30,7 @@ io.on('connection', function (socket) {
 
         socket.room = room;
         socket.join(room);
-        //console.log(io.sockets.adapter.rooms);
+        
         if (typeof io.sockets.adapter.rooms[room] === "undefined" || io.sockets.adapter.rooms[room].length <= 1) {
             console.log("New Room: ", room)
             socket.isHost = true;
@@ -39,38 +39,20 @@ io.on('connection', function (socket) {
 
             console.log("Guest joining: ", room)
             socket.isHost = false;
-            //socket.emit("guest", { room: room });
+
             let lastPacket = lastBroadcastedData[room];
             socket.emit("guest", lastPacket);
         }
     });
 
-    // socket.on('sync', function(id, msg) {
-    //     console.log("Broadcasting " + msg + " to " + id);
-    //     socket.broadcast.emit('sync', msg);
-
-    //     //socket.broadcast.to(id).emit('sync', msg);
-    //     // io.to(id).emit('sync', msg);
-    // });
-
     socket.on('sync', function(data) {
         if (socket.isHost) {
-
+            
             // TODO: Convert timestamp to server time
             lastBroadcastedData[socket.room] = data;
 
             socket.broadcast.emit('sync', data);
         }
     });
-
-    // if (socket.lastDataBroadcasted != null) {
-    //     socket.emit('update', socket.lastDataBroadcasted);
-    //     // if (socket.lastDataBroadcasted['paused']) {
-    //     //     socket.emit('sync', socket.lastDataBroadcasted);
-    //     // } else {
-
-    //     // }
-    // }
-
 
 });
